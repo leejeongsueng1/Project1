51966b76cb7d40d2d5ab7a92d071b8da476c4462
leejeongsueng1 <jki1256@hanamail.net>
2022-03-03 18:18:21 +0900

    디자인 수정버전

--- canteen/canteen/settings.py
+++ canteen/canteen/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'canteen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'webs/templates/webs')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- canteen/webs/views.py
+++ canteen/webs/views.py
@@ -13,21 +13,19 @@ host = '192.168.56.101'
 port = '3306'
 DB_name = 'project1'
 
-
-
-
 # Create your views here.
 
+#메인 인덱스 페이지 호출
 def index(request):
 
+    # 세션키를 통해 유저아이디 탐색
     session_id = request.session.session_key
     user_id = request.session.get('user')
-
 
     try:
         users = User.objects.filter(user_id = user_id)
         for u in users:
-            user_name = u.user_name
+            user_name = u.user_name 
             user_loct = u.user_loct
         context = {'user_id':user_id,
                     'session_key':session_id,
@@ -62,40 +60,28 @@ def map(request):
         # 데이터 선택 로직
         ct_name = data.drop(['소재지도로명주소', '소재지지번주소', '운영기관명', '전화번호', '급식장소', '급식대상', '급식시간','급식요일', '운영시작일자', '운영종료일자', '위도', '경도', '데이터기준일자', '제공기관코드', '제공기관명'],axis=1 )
         ct_name = ct_name.loc[:,:].values.tolist()
-        print(ct_name)  
         ct_name_list = []
-
         for i in ct_name:
             ct_name_list.append(i[0])
 
-        print(ct_name_list)
         ct_loct = data['소재지도로명주소']
         ct_loct = ct_loct.to_list()
-
         ct_tel = data['전화번호']
         ct_tel = ct_tel.to_list()
-
         ct_lat = data['위도']
         ct_lat = ct_lat.to_list()
-
         ct_lon = data['경도']
         ct_lon = ct_lon.to_list()
-
         ct_info1 = data['급식대상']
         ct_info1 = ct_info1.to_list()
-
         ct_info2 = data['급식시간']
         ct_info2 = ct_info2.to_list()
-
         ct_info3 = data['급식요일']
         ct_info3 = ct_info3.to_list()
-
         ct_info4 = data['운영종료일자']
         ct_info4 = ct_info4.to_list()
-
         ct_info5 = data['제공기관명']
         ct_info5 = ct_info5.to_list()
-        print('hi')
 
         context = {'ct_name' : ct_name_list,
                     'ct_loct' : ct_loct,
@@ -107,9 +93,6 @@ def map(request):
                     'ct_info3' : ct_info3,
                     'ct_info4' : ct_info4,
                     'ct_info5' : ct_info5}
-        print('hi')
-
-
         return render(request,'webs/map.html', context)
     except:
         users = None
