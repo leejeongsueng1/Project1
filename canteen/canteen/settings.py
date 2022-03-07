e548149e4dac112e63c512272630a3c66c7f1c50
leejeongsueng1 <jki1256@hanmail.net>
2022-03-07 13:53:38 +0900

    123

--- canteen/canteen/settings.py
+++ canteen/canteen/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'project1',
-        'USER' : 'bigdata2',
+        'USER' : 'bigdata',
         'PASSWORD' : 'Bigdata123!!',
         'HOST' : '127.0.0.1',
         'PORT' : '3306'
